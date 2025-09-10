e6cf767548d1449546dd5dbd38a8e6b4343cfc88
RashedRah1243 <rashed_rahmah@outlook.com>
2025-09-10 10:09:22 +0400

    Update project.js

--- project.js
+++ project.js
@@ -1,1 +1,19 @@
+const students = [
+  {id: '1', name: 'Rashed'},
+  {id: '2', name: 'Mohamed'},
+  {id: '3', name: 'Ahmed'},
+  {id: '4', name: 'Saeed'},
+  {id: '5', name: 'Hazza'},
+  {id: '6', name: 'Ali'},
+  {id: '8', name: 'Omar'}
+]
 
+const status-list = {
+  classList: students,
+  present: [],
+  late: [],
+  Absent: []
+}
+
+let draggedStudent = null
+let draggedFrom = null
